ba3ed2b6f02fdf659d6d147e34e2bbc10927587c
Rafael Moro <rafael.moro.galindo@gmail.com>
2021-02-15 11:54:28 -0600

    Se añadio a la galeria, el zoom a las imagenes

--- src/js/utils/gallery.js
+++ src/js/utils/gallery.js
@@ -9,7 +9,11 @@ function showPhotos() {
 
         const image = document.createElement('IMG')
         image.dataset.src = `./biuld/img/thumb/${i}.jpg`
+        image.dataset.id = i
         image.alt = `Imagen ${i} de los artistas.`
+        if(window.innerWidth >= 768) {
+            image.onclick = showBigImage
+         }
         registerImage(image)
         picture.appendChild(source)
         picture.appendChild(image)
@@ -35,6 +39,52 @@ const observer = new IntersectionObserver((entries) => {
 
 const registerImage = (image) => {
     observer.observe(image)
+}
+
+function showBigImage(evento) {
+    let id = evento.target.dataset.id
+    id = parseInt(id)
+
+    //Crear imagen
+    const picture = document.createElement('PICTURE')
+    const source = document.createElement('SOURCE')
+    source.srcset = `./biuld/img/grande/${id}.webp`
+    const image = document.createElement('img')
+    image.src = `biuld/img/grande/${id}.jpg`
+    picture.appendChild(source)
+    picture.appendChild(image)
+
+    //Boton cerrar imagen
+    const closeImage = document.createElement('p')
+    closeImage.textContent = 'X'
+    closeImage.className = 'btn-cerrar'
+
+    const overlay = document.createElement('div')
+    overlay.className = 'overlay'
+    overlay.appendChild(picture)
+    overlay.appendChild(closeImage)
+
+    setTimeout(() => {
+        overlay.classList.add('active')
+        image.classList.add('active')
+        closeImage.classList.add('active')
+    }, 500)
+
+    //Evento de cerrar overlay
+    closeImage.onclick = () => {
+        overlay.remove()
+        body.classList.remove('active')
+    }
+    overlay.onclick = function() {
+        overlay.remove()
+        body.classList.remove('active')
+    }
+
+    //Mostrar HTML
+    const body = document.querySelector('body')
+    body.appendChild(overlay)
+    //Añadir clase de fijar body
+    body.classList.add('active')
 }
 
 export default showPhotos
